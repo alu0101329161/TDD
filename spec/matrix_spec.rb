3d7b09d5862ef80fd60d4ca5f514064816447ab4
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-12 22:28:08 +0000

    Prueba fallida metodo *

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -25,4 +25,10 @@ describe Matrix do
 	  expect((Matrix.new([[2,2],[2,2]]) + Matrix.new([[2,2],[2,2]])).to_s).to eq("[[4, 4], [4, 4]]")
         end
 
+	 it "Se debe poder hacer el producto por un producto escalar " do
+          expect((Matrix.new([[0,3],[0,3]]) * 2).to_s).to eq("[[0, 6], [0, 6]]")
+          expect((Matrix.new([[1,1],[1,1]]) * 3).to_s).to eq("[[3, 3], [3, 3]]")
+	  expect((Matrix.new([[2,2],[2,2]]) * 5).to_s).to eq("[[10, 10], [10, 10]]")
+        end
+
 end
